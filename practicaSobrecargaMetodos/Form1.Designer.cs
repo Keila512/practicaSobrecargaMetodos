6afce4a9ac32b5a0987e48b9db1eb8e91ed64dfb
Angel Zurita <af3392976@gmail.com>
2023-03-09 20:59:29 -0600

    Add project files.

---
+++ practicaSobrecargaMetodos/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿using System.Windows.Forms;
+
+namespace practicaSobrecargaMetodos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtboxnombre = new System.Windows.Forms.TextBox();
+            this.dtpfecha = new System.Windows.Forms.DateTimePicker();
+            this.txtboxtelefono = new System.Windows.Forms.TextBox();
+            this.txtboxcorreo = new System.Windows.Forms.TextBox();
+            this.btnAddDefault = new System.Windows.Forms.Button();
+            this.btnPathSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOMBRE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FECHA DE NACIMIENTO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TELEFONO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CORREO:";
+            // 
+            // txtboxnombre
+            // 
+            this.txtboxnombre.Location = new System.Drawing.Point(97, 32);
+            this.txtboxnombre.Name = "txtboxnombre";
+            this.txtboxnombre.Size = new System.Drawing.Size(436, 23);
+            this.txtboxnombre.TabIndex = 4;
+            // 
+            // dtpfecha
+            // 
+            this.dtpfecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpfecha.Location = new System.Drawing.Point(176, 73);
+            this.dtpfecha.Name = "dtpfecha";
+            this.dtpfecha.Size = new System.Drawing.Size(357, 23);
+            this.dtpfecha.TabIndex = 5;
+            // 
+            // txtboxtelefono
+            // 
+            this.txtboxtelefono.Location = new System.Drawing.Point(105, 110);
+            this.txtboxtelefono.Name = "txtboxtelefono";
+            this.txtboxtelefono.Size = new System.Drawing.Size(306, 23);
+            this.txtboxtelefono.TabIndex = 6;
+            // 
+            // txtboxcorreo
+            // 
+            this.txtboxcorreo.Location = new System.Drawing.Point(94, 152);
+            this.txtboxcorreo.Name = "txtboxcorreo";
+            this.txtboxcorreo.Size = new System.Drawing.Size(317, 23);
+            this.txtboxcorreo.TabIndex = 7;
+            // 
+            // btnAddDefault
+            // 
+            this.btnAddDefault.Location = new System.Drawing.Point(615, 69);
+            this.btnAddDefault.Name = "btnAddDefault";
+            this.btnAddDefault.Size = new System.Drawing.Size(156, 35);
+            this.btnAddDefault.TabIndex = 8;
+            this.btnAddDefault.Text = "Agregar ruta default";
+            this.btnAddDefault.UseVisualStyleBackColor = true;
+            this.btnAddDefault.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // btnPathSelect
+            // 
+            this.btnPathSelect.Location = new System.Drawing.Point(798, 69);
+            this.btnPathSelect.Name = "btnPathSelect";
+            this.btnPathSelect.Size = new System.Drawing.Size(167, 35);
+            this.btnPathSelect.TabIndex = 9;
+            this.btnPathSelect.Text = "Seleccionar ruta";
+            this.btnPathSelect.UseVisualStyleBackColor = true;
+            this.btnPathSelect.Click += new System.EventHandler(this.btnPathSelect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 565);
+            this.Controls.Add(this.btnPathSelect);
+            this.Controls.Add(this.btnAddDefault);
+            this.Controls.Add(this.txtboxcorreo);
+            this.Controls.Add(this.txtboxtelefono);
+            this.Controls.Add(this.dtpfecha);
+            this.Controls.Add(this.txtboxnombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtboxnombre;
+        private DateTimePicker dtpfecha;
+        private TextBox txtboxtelefono;
+        private TextBox txtboxcorreo;
+        private Button btnAddDefault;
+        private Button btnPathSelect;
+    }
+}
+
---
+++ practicaSobrecargaMetodos/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicaSobrecargaMetodos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Contacto[] a = new Contacto[1];
+        int i = 0;
+
+        // Utilizacion del metodo save sin parametros 
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        // Utilizacion del metodo save con parametros 
+        private void btnPathSelect_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "txt|*.txt";
+            if (sfd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            FileInfo f = new FileInfo(sfd.FileName);
+            Save(f);
+        }
+
+        // SOBRECARGA DE METODOS
+
+        // METODO SIN ARGUMENTOS
+        private void Save()
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Users\af339\OneDrive\Desktop\Prueba.txt", true);
+            try
+            {
+                Contacto x = new Contacto();
+
+                if (i < a.Length)
+                {
+                    x.Nombre = txtboxnombre.Text;
+                    x.FechaN = dtpfecha.Value;
+                    x.Telefono = txtboxtelefono.Text;
+                    x.Correo = txtboxcorreo.Text;
+                    a[i] = x;
+
+                    sw.WriteLine(x.ToString());
+                    sw.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("ERROR");
+            }
+            sw.Close();
+        }
+
+        // METODO CON ARGUMENTOS
+        private void Save(FileInfo f)
+        {
+            StreamWriter sw = new StreamWriter(f.FullName, true);
+            try
+            {
+                Contacto x = new Contacto();
+
+                if (i < a.Length)
+                {
+                    x.Nombre = txtboxnombre.Text;
+                    x.FechaN = dtpfecha.Value;
+                    x.Telefono = txtboxtelefono.Text;
+                    x.Correo = txtboxcorreo.Text;
+                    a[i] = x;
+
+                    sw.WriteLine(x.ToString());
+                    sw.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("ERROR");
+            }
+            sw.Close();
+        }
+    }
+}
